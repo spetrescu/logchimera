327a20cc6ac5010c9b5709de0a9e85acaa39af34
Stefan Petrescu <stefan.petrescu997@gmail.com>
2023-10-26 17:50:56 +0200

    Add initial test file and test for heterogeneity module

---
+++ tests/test_heterogeneity.py
@@ -0,0 +1,11 @@
+from logchimera.heterogeneity import _load_log_data
+
+def test_load_log_data():
+    """Test function."""
+    log_lines = ["Header1","abc"]
+    log_templates = ["Header2","abc"]
+    log_variables = ["Header3","[]"]
+    expected = [log_lines, log_templates, log_variables]
+    actual = _load_log_data(input_file="tests/test_file_logchimera_heterogeneity_load_log_data.csv")
+    print(actual)
+    assert actual == expected, "Loading data does not work properly!"

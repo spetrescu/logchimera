2581900273451c321bdda4122a1b65db9d8451e5
Stefan Petrescu <stefan.petrescu997@gmail.com>
2023-10-26 18:06:22 +0200

    Add test for parsing functionality IO sanity check

--- tests/test_heterogeneity.py
+++ tests/test_heterogeneity.py
@@ -1,4 +1,4 @@
-from logchimera.heterogeneity import _load_log_data
+from logchimera.heterogeneity import _load_log_data, _parse_logs_for_estimating_heterogeneity
 
 def test_load_log_data():
     """Test function."""
@@ -9,3 +9,12 @@ def test_load_log_data():
     actual = _load_log_data(input_file="tests/test_data/test_file_logchimera_heterogeneity_load_log_data.csv")
     print(actual)
     assert actual == expected, "Loading data does not work properly!"
+
+def test_parse_logs_for_estimating_heterogeneity():
+    """Test function."""
+    log_lines = ["mod_jk child workerEnv in error state 6", "mod_jk child workerEnv in error state 7", "mod_jk child workerEnv in error state 7"]
+    log_templates = ["mod_jk child workerEnv in error state <*>", "mod_jk child workerEnv in error state <*>", "mod_jk child workerEnv in error state <*>"]
+    expected = [[item1, item2] for item1, item2 in zip(log_lines, log_templates)]
+    actual = _parse_logs_for_estimating_heterogeneity("tests/test_data/test_file_logchimera_heterogeneity_parse_logs_for_estimating_heterogeneity.csv")
+    assert actual == expected, "Loading data does not work properly!"
+    

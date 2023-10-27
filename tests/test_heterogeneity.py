078a5ee10630fc986470267b2346e094628d4771
Stefan Petrescu <stefan.petrescu997@gmail.com>
2023-10-27 15:35:15 +0200

    Replace AssertionError custom error message for the test_parse_logs_for_estimating_heterogeneity() function

--- tests/test_heterogeneity.py
+++ tests/test_heterogeneity.py
@@ -15,5 +15,5 @@ def test_parse_logs_for_estimating_heterogeneity():
     log_templates = ["mod_jk child workerEnv in error state <*>", "mod_jk child workerEnv in error state <*>", "mod_jk child workerEnv in error state <*>"]
     expected = [[item1, item2] for item1, item2 in zip(log_lines, log_templates)]
     actual = _parse_logs_for_estimating_heterogeneity("tests/test_data/test_file_logchimera_heterogeneity_parse_logs_for_estimating_heterogeneity.csv")
-    assert actual == expected, "Loading data does not work properly!"
+    assert actual == expected, "Parsing functionality for estimating heterogeneity not working!"
     

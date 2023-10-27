4382fe2c974c088a5b7b2b9f1e07d6c4518d987c
Stefan Petrescu <stefan.petrescu997@gmail.com>
2023-10-27 15:37:55 +0200

    Add test for sanity check 2k sampling functionality

--- tests/test_heterogeneity.py
+++ tests/test_heterogeneity.py
@@ -1,4 +1,4 @@
-from logchimera.heterogeneity import _load_log_data, _parse_logs_for_estimating_heterogeneity
+from logchimera.heterogeneity import _load_log_data, _parse_logs_for_estimating_heterogeneity, _sample_2k_logs_using_parsing
 
 def test_load_log_data():
     """Test function."""
@@ -17,3 +17,11 @@ def test_parse_logs_for_estimating_heterogeneity():
     actual = _parse_logs_for_estimating_heterogeneity("tests/test_data/test_file_logchimera_heterogeneity_parse_logs_for_estimating_heterogeneity.csv")
     assert actual == expected, "Parsing functionality for estimating heterogeneity not working!"
     
+def test_sample_2k_logs_using_parsing():
+    """Test function."""
+    log_lines = ["mod_jk child workerEnv in error state 6", "mod_jk child workerEnv in error state 7", "mod_jk child workerEnv in error state 7"]
+    log_templates = ["mod_jk child workerEnv in error state <*>", "mod_jk child workerEnv in error state <*>", "mod_jk child workerEnv in error state <*>"]
+    log_data = [[item1, item2] for item1, item2 in zip(log_lines, log_templates)]
+    expected = log_data
+    actual = _sample_2k_logs_using_parsing(log_data)
+    assert actual == expected, "Sampling functionality for estimating heterogeneity using log parsing not working!"

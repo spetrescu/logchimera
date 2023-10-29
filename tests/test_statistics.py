6e965c54b58e753568e7b7facc4dbf70f30cff22
Stefan Petrescu <stefan.petrescu997@gmail.com>
2023-10-29 17:34:05 +0100

    Add initial test for computing no unique chars

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -1,4 +1,4 @@
-from logchimera.statistics import compute_no_unique_words
+from logchimera.statistics import compute_no_unique_words, compute_no_unique_chars
 
 def test_compute_no_unique_words():
     """Test function for computing unique number of words"""
@@ -6,3 +6,10 @@ def test_compute_no_unique_words():
     expected = 4
     actual = compute_no_unique_words(log_lines=log_lines)
     assert actual == expected, "Function for computing unique number of words in statistics module is not working!"
+
+def test_compute_no_unique_chars():
+    """Test function for computing unique number of characters"""
+    log_lines = ["Log line 1", "Log line 2"] # there should be 10 chars
+    expected = 10
+    actual = compute_no_unique_chars(log_lines=log_lines)
+    assert actual == expected, "Function for computing unique number of characters in statistics module is not working!"

850072d854aa4c05c427a69a75fc599791c41a41
Stefan Petrescu <stefan.petrescu997@gmail.com>
2023-11-16 16:42:31 +0100

    Refactor test structure for parser module

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,7 +4,7 @@ def test_parsing_logs():
     """Test function for parsing logs function."""
     expected = [['mod_jk child workerEnv in error state 6', 'mod_jk child workerEnv in error state <*>'],
                 ['mod_jk child workerEnv in error state 7', 'mod_jk child workerEnv in error state <*>']]
-    actual = parse_log_lines(file_path="tests/test_data_parser/test_file_initial_parser_functionality.csv")
+    actual = parse_log_lines(file_path="tests/test_data/parser_module/test_file_initial_parser_functionality.csv")
     actual = actual[["Content", "EventTemplate"]]
     actual = actual.values.tolist()
     assert actual == expected, "Sanity check parsing functionality is not working!"
@@ -12,7 +12,7 @@ def test_parsing_logs():
 def test_parsing_logs_number_of_logs():
     """Test function for checking if parsing returns the same number of logs."""
     expected = 3000
-    actual = parse_log_lines(file_path="tests/test_data_parser/test_file_parser_for_checking_number_logs_parsed.csv")
+    actual = parse_log_lines(file_path="tests/test_data/parser_module/test_file_parser_for_checking_number_logs_parsed.csv")
     actual = actual[["Content", "EventTemplate"]]
     actual = len(actual.values.tolist())
     assert actual == expected, "Parser does not return the same number of logs as in the input file!"

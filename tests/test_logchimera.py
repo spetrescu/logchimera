92e08a6677a1231b85f85713720a196d07a25898
Stefan Petrescu <stefan.petrescu997@gmail.com>
2023-11-16 18:02:56 +0100

    Add test for character upper bound limit in estimating heterogeneity

--- tests/test_logchimera.py
+++ tests/test_logchimera.py
@@ -11,3 +11,9 @@ def test_estimate_heterogeneity_for_file_with_more_words_than_upper_limit_for_he
     expected = 0.446
     actual = estimate_heterogeneity(file_path="tests/test_data/logchimera_module/test_file_logchimera_for_upper_bound_word_limit_heterogeneity_estimation.csv")
     assert actual == expected, "Estimating heterogeneity function in logchimera module is not working for the case when the data contains more words than the upper bound limit!"
+
+def test_estimate_heterogeneity_for_file_with_more_chars_than_upper_limit_for_heterogeneity():
+    """Testing heterogeneity estimation functionality when file has more characters than upper bound."""
+    expected = 0.208
+    actual = estimate_heterogeneity(file_path="tests/test_data/logchimera_module/test_file_logchimera_for_upper_bound_character_limit_heterogeneity_estimation.csv")
+    assert actual == expected, "Estimating heterogeneity function in logchimera module is not working for the case when the data contains more characters than the upper bound limit!"

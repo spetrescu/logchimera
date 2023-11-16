4cdcc7079cc8b9c280362fb4acd037ce99132dde
Stefan Petrescu <stefan.petrescu997@gmail.com>
2023-11-16 16:25:48 +0100

    Add test to check if input no logs equals no logs parsed

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -7,4 +7,12 @@ def test_parsing_logs():
     actual = parse_log_lines(file_path="tests/test_data_parser/test_file_initial_parser_functionality.csv")
     actual = actual[["Content", "EventTemplate"]]
     actual = actual.values.tolist()
-    assert actual == expected, "Test function is not working!"
+    assert actual == expected, "Sanity check parsing functionality is not working!"
+
+def test_parsing_logs_number_of_logs():
+    """Test function for checking if parsing returns the same number of logs."""
+    expected = 3000
+    actual = parse_log_lines(file_path="tests/test_data_parser/test_file_parser_for_checking_number_logs_parsed.csv")
+    actual = actual[["Content", "EventTemplate"]]
+    actual = len(actual.values.tolist())
+    assert actual == expected, "Parser does not return the same number of logs as in the input file!"

b533a3e56c7f0a11cf890340e596db01d4a0514b
Stefan Petrescu <stefan.petrescu997@gmail.com>
2023-11-16 17:05:14 +0100

    Add sanity check test for heterogeneity estimation

--- tests/test_logchimera.py
+++ tests/test_logchimera.py
@@ -1,7 +1,7 @@
-from logchimera.logchimera import function_test
+from logchimera.logchimera import estimate_heterogeneity
 
-def test_function_test():
-    """Test function."""
-    expected = ""
-    actual = function_test(test_string="")
-    assert actual == expected, "Test function is not working!"
+def test_estimate_heterogeneity():
+    """Test estimating heterogeneity sanity check."""
+    expected = 0.01
+    actual = estimate_heterogeneity(file_path="tests/test_data/logchimera_module/test_file_logchimera_for_estimating_heterogeneity.csv")
+    assert actual == expected, "Estimating heterogeneity function in logchimera module is not working!"
